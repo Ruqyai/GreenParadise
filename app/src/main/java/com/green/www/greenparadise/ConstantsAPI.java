7e002a4d2c314032fe264d55ba2812c3f31355d9
Ruqiya <Seu0students@gmail.com>
2018-08-02 08:32:48 +0300

    adding a class for API

---
+++ app/src/main/java/com/green/www/greenparadise/ConstantsAPI.java
@@ -0,0 +1,30 @@
+package com.green.www.greenparadise;
+
+/**
+ * Created by hp on 8/2/2018.
+ */
+
+public class ConstantsAPI {
+    private static final String ROOT_URL = "http://localhost/hajjapp/";
+
+    public static final String URL_ADD_USER = ROOT_URL+"Users/add";
+    public static final String URL_VIEW_USER = ROOT_URL+"Users/view/";
+    public static final String URL_UPDATE_USER = ROOT_URL+"Users/update/";
+    public static final String URL_DELETE_USER = ROOT_URL+"Users/ajax_delete/";
+
+    public static final String URL_ADD_BUYER = ROOT_URL+"Buyers/add";
+    public static final String URL_VIEW_BUYER = ROOT_URL+"Buyers/view/";
+    public static final String URL_UPDATE_BUYER = ROOT_URL+"Buyers/update/";
+    public static final String URL_DELETE_BUYER = ROOT_URL+"Buyers/ajax_delete/";
+
+    public static final String URL_ADD_PARTNER = ROOT_URL+"Partners/add";
+    public static final String URL_VIEW_PARTNER = ROOT_URL+"Partners/view/";
+    public static final String URL_UPDATE_PARTNER = ROOT_URL+"Partners/update/";
+    public static final String URL_DELETE_PARTNER = ROOT_URL+"Partners/ajax_delete/";
+
+    public static final String URL_ADD_RASH_BIN = ROOT_URL+"Trashbins/add";
+    public static final String URL_VIEW_TRASH_BIN = ROOT_URL+"Trashbins/view/";
+    public static final String URL_UPDATE_RASH_BIN = ROOT_URL+"Trashbins/update/";
+    public static final String URL_DELETE_RASH_BIN = ROOT_URL+"Trashbins/ajax_delete/";
+
+}
