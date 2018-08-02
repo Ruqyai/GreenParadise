545759ed9e8d39b40adeb91b7c7dcbeba064d4cb
Ruqiya <Seu0students@gmail.com>
2018-08-02 21:59:48 +0300

    re-change main activty

--- app/src/main/java/com/green/www/greenparadise/MapsActivity.java
+++ app/src/main/java/com/green/www/greenparadise/MapsActivity.java
@@ -7,8 +7,12 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -43,4 +47,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+
+/*
+    ArrayList<MarkerData> markersArray = new ArrayList<MarkerData>();
+
+for(int i = 0 ; i < markersArray.size() ; i++) {
+
+        createMarker(markersArray.get(i).getLatitude(), markersArray.get(i).getLongitude(), markersArray.get(i).getTitle(), markersArray.get(i).getSnippet(), markersArray.get(i).getIconResID());
+    }
+
+
+    protected Marker createMarker(double latitude, double longitude, String title, String snippet, int iconResID) {
+
+        return googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .anchor(0.5f, 0.5f)
+                .title(title)
+                .snippet(snippet)
+
+                .icon(BitmapDescriptorFactory.fromResource(iconResID)));
+    }
+*/
+
 }
