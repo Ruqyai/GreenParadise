a7ebc84aa7014a86b4a4824fc13c0c120d87bf87
Ruqiya <Seu0students@gmail.com>
2018-08-03 00:48:53 +0300

    new icon main activty

--- app/src/main/java/com/green/www/greenparadise/LoginActivity.java
+++ app/src/main/java/com/green/www/greenparadise/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.green.www.greenparadise;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -23,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
+                //startActivity(new Intent(this, BuyerActivity.class));
                 case R.id.navigation_notifications:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
@@ -39,6 +42,9 @@ public class LoginActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Button btn = (Button)findViewById(R.id.email_sign_in_button);
+        //btn.setOnClickListener(startActivity(new Intent(this, MainActivity.class) );));
+
     }
 
 }
